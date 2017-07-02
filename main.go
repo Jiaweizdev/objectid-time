7c0fb16dbc6aef1a8539a17ed6ed0bfabee487c6
Jiawei Zhang <jiaweizdev@gmail.com>
2017-07-02 21:13:51 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/mgo.v2/bson"
+	"os"
+)
+
+func main() {
+
+	// Expect a ObjectId passed as the first CLI parameter
+	input := os.Args[1]
+
+	// Check if it is a valid ObjectID
+	if bson.IsObjectIdHex(input) {
+		extractDateFromObjectId(bson.ObjectIdHex(input))
+	} else {
+		fmt.Println("The input is not a valid ObjectId.")
+	}
+}
+
+func extractDateFromObjectId(id bson.ObjectId) {
+
+	// Time in UTC
+	fmt.Println(id.Time().UTC())
+	// Time in Local Timezone
+	fmt.Println(id.Time())
+}
